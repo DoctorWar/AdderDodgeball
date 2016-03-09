c10e3fb0feb54cc5451e24b7eac68f2a4bcf67b9
C T <diggermoth@gmail.com>
2016-03-09 12:11:58 -0500

    AI evade-suicide AI should run from the ball when it's moving, and chase after it when it's not moving. They currently die instead of picking up the ball. Lens flare off

--- Source/Adder_DodgeBall/AI_Character.cpp
+++ Source/Adder_DodgeBall/AI_Character.cpp
@@ -2,7 +2,8 @@
 
 #include "Adder_DodgeBall.h"
 #include "AI_Character.h"
-#include "EngineUtils.h"
+//#include "EngineUtils.h"
+#include "Engine.h"
 
 // Sets default values
 AAI_Character::AAI_Character()
@@ -37,8 +38,8 @@ void AAI_Character::BeginPlay()
 	if (targetRegion->IsValidLowLevel() && targetRegion != nullptr) {
 		targetRegion->GetActorBounds(false, boundsPoint, boundsVector);
 		//saves dividing by 2 every frame..
-		boundsVector.X *= 0.5;
-		boundsVector.Y *= 0.5;
+		//boundsVector.X *= 0.5;
+		//boundsVector.Y *= 0.5;
 	}
 }
 
@@ -49,27 +50,40 @@ void AAI_Character::Tick( float DeltaTime )
 	AITimer = AITimer - DeltaTime;
 	if (AITimer < 0) {
 		targetPoint += FVector(rand() % 511 - 255, rand() % 511 - 255, 0);
+		//UE_LOG(LogTemp, Warning, TEXT("Iterating"));
+		for (TActorIterator<AActor> Tai(GetWorld()); Tai; ++Tai) {
+
+			if (Tai->ActorHasTag("Ball")) {
+				//chase the ball if it's on AI side of the court
+				FVector BallPoint = Tai->GetActorLocation();
+				if (BallPoint.X < boundsPoint.X + boundsVector.X && BallPoint.X > boundsPoint.X - boundsVector.X
+					&&BallPoint.Y < boundsPoint.Y + boundsVector.Y && BallPoint.Y > boundsPoint.Y - boundsVector.Y) {
+					targetPoint = BallPoint;
+				}
+				//run from the ball if it's flying
+				FVector BallVelocity = Tai->GetVelocity();
+				//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("BallVector: %f"), BallVelocity.Size()));
+				if (BallVelocity.Size() > 80.0f) {
+					FVector UserPoint = GetTransform().GetLocation();
+					FVector MovePoint = (GetTransform().GetLocation() - BallPoint);
+					MovePoint.Normalize();
+					targetPoint = UserPoint + (MovePoint * 180);
+				}
+			}
+		}
+		targetPoint.Z = GetTransform().GetLocation().Z;
 		if (targetPoint.X > boundsPoint.X + boundsVector.X) targetPoint.X = boundsPoint.X + boundsVector.X;
 		if (targetPoint.X < boundsPoint.X - boundsVector.X) targetPoint.X = boundsPoint.X - boundsVector.X;
 		if (targetPoint.Y > boundsPoint.Y + boundsVector.Y) targetPoint.Y = boundsPoint.Y + boundsVector.Y;
 		if (targetPoint.Y < boundsPoint.Y - boundsVector.Y) targetPoint.Y = boundsPoint.Y - boundsVector.Y;
 		AITimer = AITimer + AITimeStep;
 	}
+
 	if (FMath::Abs(FVector::Dist(targetPoint, GetTransform().GetLocation())) > 32) {
 		GetMovementComponent()->AddInputVector(targetPoint - GetTransform().GetLocation(), false);
 	}
 
-	UE_LOG(LogTemp, Warning, TEXT("Iterating"));
-	for (TActorIterator<AActor> Tai(GetWorld()); Tai; ++Tai) {
 
-		if (Tai->ActorHasTag("Ball")) {
-			FVector BallPoint = Tai->GetActorLocation();
-			if (BallPoint.X < boundsPoint.X + boundsVector.X && BallPoint.X > boundsPoint.X - boundsVector.X
-				&&BallPoint.Y < boundsPoint.Y + boundsVector.Y && BallPoint.Y > boundsPoint.Y - boundsVector.Y) {
-				targetPoint = BallPoint;
-			}
-		}
-	}
 
 }
 
